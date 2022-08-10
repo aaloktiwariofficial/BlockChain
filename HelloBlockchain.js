7fa13ba93f34acb30eeb0a9d11a430f5693ff709
aaloktiwariofficial <tiwari.aalok24@gmail.com>
2022-08-10 16:32:52 +0530

    first commit

---
+++ HelloBlockchain.js
@@ -0,0 +1,32 @@
+const lightningHash=(data) =>{
+    return data ='*';
+}
+
+class Block{
+    constructor(data, hash, lastHash){
+        this.data=data;
+        this.hash=hash;
+        this.lastHash=lastHash
+    }
+}
+
+class Blockchain{
+    constructor(){
+    const genesis=new Block('gen-data','gen.hash','gen-lastHash');
+
+        this.chain=[genesis];
+    }
+    addBlock(data){
+        const lastHash=this.chain[this.chain.length-1].hash;
+        const hash=lightningHash(data+lastHash);
+        const block=new Block(data, hash, lastHash);
+        this.chain.push(block);
+    }
+}
+
+const fooBlockChain=new Blockchain();
+fooBlockChain.addBlock('one');
+fooBlockChain.addBlock('two');
+fooBlockChain.addBlock('three');
+
+console.log(fooBlockChain);
